9362bb5d1a7601450e28193ed35014a2e3478d2b
Valentina Zantedeschi <vzantedeschi@gmail.com>
2017-03-01 15:30:33 +0100

    clean useless imports

--- src/landmark.py
+++ src/landmark.py
@@ -16,12 +16,3 @@ def pca_landmarks(x,n):
     pca = PCA(n).fit(x)
 
     return pca.components_
-
-def get_unit_vectors(landmarks,clusterer=None):
-    if clusterer is not None:
-        land_clusters = clusterer.predict(landmarks)
-        centroids = clusterer.cluster_centers_
-        land_centroids = centroids[land_clusters]
-    else:
-        land_centroids = np.zeros(landmarks.shape)
-    return csr_matrix(normalize(landmarks-land_centroids).transpose())
